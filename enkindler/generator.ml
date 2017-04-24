0fd4ad847cc1b4236e902617bee28ccacdea59a8
octachron <octa@polychoron.fr>
2017-04-24 09:28:42 +0200

    Polymorphic variant option

--- enkindler/generator.ml
+++ enkindler/generator.ml
@@ -11,34 +11,40 @@ module Enum = struct
     | (_, Ctype.Abs n) :: q -> range n n q
     | _ -> None
 
-  let def ppf name constrs =
-    let constr ppf (c, _) = Fmt.pf ppf "|%s" c in 
+  type implementation = Std | Poly
+  let pp_constr impl ppf (c, _ ) =
+    match impl with
+    | Std -> Fmt.pf ppf "%s" c
+    | Poly -> Fmt.pf ppf "`%s" c
+
+  let def impl ppf name constrs =
+    let constr ppf c = Fmt.pf ppf "|%a" (pp_constr impl) c in
     Fmt.pf ppf "type %s =\n%a\n" name
     (Fmt.list ~sep:(fun ppf () -> Fmt.pf ppf "\n") constr) constrs
 
-  let to_int ppf _name constrs =
+  let to_int impl ppf _name constrs =
     Fmt.pf ppf "let to_int = function\n";
     let constr ppf = function
-      | (c, Ctype.Abs n) -> Fmt.pf ppf "| %s -> %d\n" c n
+      | (_, Ctype.Abs n as c) -> Fmt.pf ppf "| %a -> %d\n" (pp_constr impl) c n
       | _ -> () in
-    Fmt.list ~sep:(fun ppf () -> Fmt.pf ppf "\n") constr ppf constrs 
+    Fmt.list ~sep:(fun ppf () -> Fmt.pf ppf "\n") constr ppf constrs
 
-  let of_int ppf _name constrs =
+  let of_int impl ppf _name constrs =
     Fmt.pf ppf "let of_int = function\n";
     let constr ppf = function
-      | (c, Ctype.Abs n) -> Fmt.pf ppf "| %d -> %s\n" n c
+      | (_, Ctype.Abs n as c) -> Fmt.pf ppf "| %d -> %a\n" n (pp_constr impl) c
       | _ -> () in
-    Fmt.list ~sep:(fun ppf () -> Fmt.pf ppf "\n") constr ppf constrs 
+    Fmt.list ~sep:(fun ppf () -> Fmt.pf ppf "\n") constr ppf constrs
 
   let view ppf () =
-    Fmt.pf ppf "let view = Ctype.view to_int of_int int\n" 
+    Fmt.pf ppf "let view = Ctype.view to_int of_int int\n"
 
-  let make_module ppf name constrs =
+  let make_module impl ppf name constrs =
     Fmt.pf ppf "module %s = struct\n" name;
-    def ppf name constrs;
-    to_int ppf name constrs;
-    of_int ppf name constrs;
+    def impl ppf name constrs;
+    to_int impl ppf name constrs;
+    of_int impl ppf name constrs;
     view ppf ();
     Fmt.pf ppf "end\n"
-  
+
 end
