adebd8c421e89b05f27d5535bb045aca633a33eb
octachron <octa@polychoron.fr>
2017-04-24 11:34:02 +0200

    Record skeleton

--- enkindler/generator.ml
+++ enkindler/generator.ml
@@ -1,3 +1,5 @@
+let sep ppf () = Fmt.pf ppf "\n"
+
 module Enum = struct
 
   let contiguous_range =
@@ -20,31 +22,65 @@ module Enum = struct
   let def impl ppf name constrs =
     let constr ppf c = Fmt.pf ppf "|%a" (pp_constr impl) c in
     Fmt.pf ppf "type %s =\n%a\n" name
-    (Fmt.list ~sep:(fun ppf () -> Fmt.pf ppf "\n") constr) constrs
+    (Fmt.list ~sep constr) constrs
 
   let to_int impl ppf _name constrs =
     Fmt.pf ppf "let to_int = function\n";
     let constr ppf = function
       | (_, Ctype.Abs n as c) -> Fmt.pf ppf "| %a -> %d\n" (pp_constr impl) c n
       | _ -> () in
-    Fmt.list ~sep:(fun ppf () -> Fmt.pf ppf "\n") constr ppf constrs
+    Fmt.list ~sep constr ppf constrs
 
   let of_int impl ppf _name constrs =
     Fmt.pf ppf "let of_int = function\n";
     let constr ppf = function
       | (_, Ctype.Abs n as c) -> Fmt.pf ppf "| %d -> %a\n" n (pp_constr impl) c
       | _ -> () in
-    Fmt.list ~sep:(fun ppf () -> Fmt.pf ppf "\n") constr ppf constrs
+    Fmt.list ~sep constr ppf constrs
 
   let view ppf () =
-    Fmt.pf ppf "let view = Ctype.view to_int of_int int\n"
+    Fmt.pf ppf "let view = Ctype.view ~write:to_int ~read:of_int int\n"
 
-  let make_module impl ppf name constrs =
+  let make impl ppf name constrs =
     Fmt.pf ppf "module %s = struct\n" name;
     def impl ppf name constrs;
     to_int impl ppf name constrs;
     of_int impl ppf name constrs;
     view ppf ();
-    Fmt.pf ppf "end\n"
+    Fmt.pf ppf "end\n";
+    Fmt.pf ppf "let %s = %s.view \n" name name
 
+end
+
+module Typexp = struct
+  let rec pp ppf = function
+    | Ctype.Const t -> pp ppf t
+    | Ctype.Name n -> Fmt.pf ppf "%s" n
+    | Ctype.Ptr typ -> Fmt.pf ppf "(ptr @@ %a )" pp typ
+    | Ctype.String -> Fmt.pf ppf "string"
+    | Ctype.Array (_,typ) -> Fmt.pf ppf "(ptr @@ %a )" pp typ
+    | Ctype.Enum _ | Record _ | Union _ | Bitset _ | Bitfields _ ->
+      failwith "Anonymous type"
+    | Ctype.Handle _ | Result _ | FunPtr _ ->
+      failwith "Not_implemented"
+end
+
+module Record = struct
+
+  let field name ppf (field_name,typ)=
+    Fmt.pf ppf "let %s = field \"%s\" %a"
+      field_name name Typexp.pp typ
+
+  let def ppf name fields =
+    Fmt.pf ppf "type t\n";
+    Fmt.pf ppf "let t: t structure typ = structure \"%s\"\n"
+      name;
+    Fmt.list ~sep (field name) ppf fields;
+    Fmt.pf ppf "let () = Ctype.seal t\n"
+
+  let make ppf name fields =
+    Fmt.pf ppf "module %s = struct\n" name;
+    def ppf name fields;
+    Fmt.pf ppf "end\n";
+    Fmt.pf ppf "let %s = %s.t \n" name
 end
