044da8a3afb3b4e8c0b0e4c24035cbc80537a962
octachron <octa@polychoron.fr>
2017-04-25 16:49:33 +0200

    Forgotten gen main

---
+++ enkindler/gen.ml
@@ -0,0 +1,12 @@
+
+let read filename =
+  let spec = open_in filename in
+  let source = Xmlm.(make_input @@ `Channel spec) in
+  Typed.typecheck @@ Xml.normalize @@ snd @@ Xml.tree source
+
+let () =
+  let info = read Sys.argv.(1) in
+  let output = Format.formatter_of_out_channel
+    @@ open_out Sys.argv.(2) in
+  List.iter (Generator.make output)
+  @@ Misc.StringMap.bindings info.entities
