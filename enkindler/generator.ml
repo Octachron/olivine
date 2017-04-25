9ec56677745d2dbde78127585308be4281204db9
octachron <octa@polychoron.fr>
2017-04-25 15:47:19 +0200

    Code generator improvement

--- enkindler/generator.ml
+++ enkindler/generator.ml
@@ -14,41 +14,47 @@ module Enum = struct
     | _ -> None
 
   type implementation = Std | Poly
-  let pp_constr impl ppf (c, _ ) =
+  let pp_constr name impl ppf (c, _ ) =
+    let name = let open Name_study in
+      remove_prefix name (path c) in
     match impl with
-    | Std -> Fmt.pf ppf "%s" c
-    | Poly -> Fmt.pf ppf "`%s" c
+    | Std -> Fmt.pf ppf "%a" Name_study.pp_constr name
+    | Poly -> Fmt.pf ppf "`%a" Name_study.pp_constr name
 
   let def impl ppf name constrs =
-    let constr ppf c = Fmt.pf ppf "|%a" (pp_constr impl) c in
-    Fmt.pf ppf "type %s =\n%a\n" name
-    (Fmt.list ~sep constr) constrs
+    let constr ppf c = Fmt.pf ppf "    | %a" (pp_constr name impl) c in
+    Fmt.pf ppf "  type t =\n%a" (Fmt.list ~sep constr) constrs
 
-  let to_int impl ppf _name constrs =
-    Fmt.pf ppf "let to_int = function\n";
+  let to_int impl ppf name constrs =
+    Fmt.pf ppf "\n  let to_int = function\n";
     let constr ppf = function
-      | (_, Ctype.Abs n as c) -> Fmt.pf ppf "| %a -> %d\n" (pp_constr impl) c n
+      | (_, Ctype.Abs n as c) ->
+        Fmt.pf ppf "    | %a -> %d" (pp_constr name impl) c n
       | _ -> () in
     Fmt.list ~sep constr ppf constrs
 
-  let of_int impl ppf _name constrs =
-    Fmt.pf ppf "let of_int = function\n";
+  let of_int impl ppf name constrs =
+    Fmt.pf ppf "\n  let of_int = function\n";
     let constr ppf = function
-      | (_, Ctype.Abs n as c) -> Fmt.pf ppf "| %d -> %a\n" n (pp_constr impl) c
+      | (_, Ctype.Abs n as c) ->
+        Fmt.pf ppf "    | %d -> %a" n (pp_constr name impl) c
       | _ -> () in
     Fmt.list ~sep constr ppf constrs
 
   let view ppf () =
-    Fmt.pf ppf "let view = Ctype.view ~write:to_int ~read:of_int int\n"
+    Fmt.pf ppf
+      "\n  let view = Ctype.view ~write:to_int ~read:of_int int\n"
 
   let make impl ppf name constrs =
-    Fmt.pf ppf "module %s = struct\n" name;
+    let name = Name_study.path name in
+    Fmt.pf ppf "module %a = struct\n" Name_study.pp_module name;
     def impl ppf name constrs;
     to_int impl ppf name constrs;
     of_int impl ppf name constrs;
     view ppf ();
     Fmt.pf ppf "end\n";
-    Fmt.pf ppf "let %s = %s.view \n" name name
+    Fmt.pf ppf "let %a = %a.view\n"
+      Name_study.pp_var name Name_study.pp_module name
 
 end
 
@@ -56,9 +62,9 @@ module Typexp = struct
   let rec pp ppf = function
     | Ctype.Const t -> pp ppf t
     | Ctype.Name n -> Fmt.pf ppf "%s" n
-    | Ctype.Ptr typ -> Fmt.pf ppf "(ptr @@ %a )" pp typ
+    | Ctype.Ptr typ -> Fmt.pf ppf "(ptr @@ %a)" pp typ
     | Ctype.String -> Fmt.pf ppf "string"
-    | Ctype.Array (_,typ) -> Fmt.pf ppf "(ptr @@ %a )" pp typ
+    | Ctype.Array (_,typ) -> Fmt.pf ppf "( ptr @@ %a )" pp typ
     | Ctype.Enum _ | Record _ | Union _ | Bitset _ | Bitfields _ ->
       failwith "Anonymous type"
     | Ctype.Handle _ | Result _ | FunPtr _ ->
@@ -68,19 +74,46 @@ end
 module Record = struct
 
   let field name ppf (field_name,typ)=
-    Fmt.pf ppf "let %s = field \"%s\" %a"
-      field_name name Typexp.pp typ
+    let field_name =
+      Name_study.(remove_prefix name @@ path field_name) in
+    Fmt.pf ppf "  let %a = field \"%a\" %a"
+      Name_study.pp_var field_name Name_study.pp_type name
+      Typexp.pp typ
 
   let def ppf name fields =
-    Fmt.pf ppf "type t\n";
-    Fmt.pf ppf "let t: t structure typ = structure \"%s\"\n"
-      name;
+    Fmt.pf ppf "  type t\n";
+    Fmt.pf ppf "  let t: t structure typ = structure \"%a\"\n"
+      Name_study.pp_type name;
     Fmt.list ~sep (field name) ppf fields;
-    Fmt.pf ppf "let () = Ctype.seal t\n"
+    Fmt.pf ppf "\n  let () = Ctype.seal t\n"
 
   let make ppf name fields =
-    Fmt.pf ppf "module %s = struct\n" name;
+    let name = Name_study.path name in
+    Fmt.pf ppf "module %a = struct\n" Name_study.pp_module name;
     def ppf name fields;
     Fmt.pf ppf "end\n";
-    Fmt.pf ppf "let %s = %s.t \n" name
+    Fmt.pf ppf "let %a = %a.t\n"
+      Name_study.pp_var name Name_study.pp_module name
+
 end
+
+let make_type ppf name = function
+  | Ctype.Const _ | Name _ | Ptr _ | String | Array (_,_)
+  | Result _ -> ()
+
+  | FunPtr _ -> Fmt.(pf stderr) "@{<red> FunPtr not implemented@}@."
+  | Union _ -> Fmt.(pf stderr) "@{<red> Union not implemented@}@."
+  | Bitset _ -> Fmt.(pf stderr) "@{<red> Bitset not implemented@}@."
+  | Bitfields _ -> Fmt.(pf stderr) "@{<red> Bitset not implemented@}@."
+  | Handle _ ->  Fmt.(pf stderr) "@{<red> Handle@}@."
+
+  | Enum constrs ->
+    Enum.make Enum.Std ppf name constrs
+  | Record r ->
+    Record.make ppf name r.fields
+
+let make ppf (name,obj)=
+  match obj with
+  | Typed.Type t -> make_type ppf name t
+  | Fn _f -> ()
+  | Const _c -> ()
