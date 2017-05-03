e1a63b742c95594ba363e2b0246d6bc052ce4d6f
octachron <octa@polychoron.fr>
2017-05-03 19:27:14 +0200

    Fix c code

--- c/base.c
+++ c/base.c
@@ -1,21 +1,39 @@
 #include <vulkan/vulkan.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void test(VkResult r) {
+  if (r < 0) {
+      printf("Failed with error code %d\n",r);
+      exit(1);
+    }
+}
+
 int main(int nargs, char** args) {
 
+  uint32_t nExtensions= 0;
+  const char *extensionNames[0] = {};
 
-const char *extensionNames[] = { "VK_KHR_surface", "VK_KHR_win32_surface" };
-
-VkInstanceCreateInfo instanceCreateInfo = {
-  VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // VkStructureType sType;
-  NULL, // const void* pNext;
-  0,    // VkInstanceCreateFlags flags;
-  NULL, // const VkApplicationInfo* pApplicationInfo;
-  0,    // uint32_t enabledLayerNameCount;
-  NULL, // const char* const* ppEnabledLayerNames;
-  2,    // uint32_t enabledExtensionNameCount;
-  extensionNames, // const char* const* ppEnabledExtensionNames;
-};
+  VkInstanceCreateInfo instanceCreateInfo = {
+    VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // VkStructureType sType;
+    NULL, // const void* pNext;
+    0,    // VkInstanceCreateFlags flags;
+    NULL, // const VkApplicationInfo* pApplicationInfo;
+    0,    // uint32_t enabledLayerNameCount;
+    NULL, // const char* const* ppEnabledLayerNames;
+    nExtensions,    // uint32_t enabledExtensionNameCount;
+    extensionNames, // const char* const* ppEnabledExtensionNames;
+  };
 
  VkInstance c;
- VkResult r = VkCreateInstance(&instanceCreateInfo, NULL, &c);
+ VkResult r = vkCreateInstance(&instanceCreateInfo, NULL, &c);
+ test(r);
+
+ int count = 5;
+ VkPhysicalDevice devices[count];
+
+ r = vkEnumeratePhysicalDevices(c,&count,devices);
+ test(r);
+ printf ("Found %d devices\n", count);
 
 }
