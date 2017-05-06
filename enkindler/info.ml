4813b07ab5f2ad8ee4ee2b41f6f844f3b0eac49b
octachron <octa@polychoron.fr>
2017-05-06 11:42:01 +0200

    Update info

--- enkindler/info.ml
+++ enkindler/info.ml
@@ -11,13 +11,17 @@ let read filename =
   Typed.typecheck @@ Xml.normalize @@ snd @@ Xml.tree source
 
 let () =
-  (*  if Array.length Sys.argv > 2 then *)
-    let info = read Sys.argv.(1) in
-    (*    let query = Sys.argv.(2) in *)
-(*    let counts = Name_study.count_names info.entities in
+  (*   then *)
+  let info = read Sys.argv.(1) in
+  if Array.length Sys.argv > 2 then
+    let query = Sys.argv.(2) in
+    Typed.pp_entity stdout @@ (query, N.find query info.entities)
+  else
+    Typed.pp stdout info
+
+    (*    let counts = Name_study.count_names info.entities in
     let pp_count ppf (name,n) = fp ppf "%s(%d)" name n in
     let binds = List.filter (fun x -> snd x > 4) (N.bindings counts) in
       fp stdout "@[<v>%a@]@." (Fmt.list pp_count) binds *)
     (* fp stdout "%a@." pp_nametree  (nametree info.entities) *)
-    (*    Typed.pp_entity stdout @@ (query, N.find query info.entities)*)
-    Typed.pp stdout info
+    (*   *)
