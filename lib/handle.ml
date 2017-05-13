5d489e267fa7d0cb10fb5f9adf16077fef40f3a2
octachron <octa@polychoron.fr>
2017-05-14 01:49:44 +0200

    Add null handle

--- lib/handle.ml
+++ lib/handle.ml
@@ -2,6 +2,7 @@ module type S = sig
   type t
   val t: t Ctypes.typ
   val view: t Ctypes.typ
+  val null: t
 end
 module Make(): S =
 struct
@@ -9,4 +10,5 @@ struct
   type t = self Ctypes.structure Ctypes.ptr
   let t: t Ctypes.typ = Ctypes.ptr (Ctypes.structure "")
   let view = t
+  let null = Ctypes.(coerce @@ ptr void) t Ctypes.null
 end
