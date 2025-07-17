623a50a28c372e825cf6239b554229e0187e33e6
Thomas Leonard <talex5@gmail.com>
2025-07-17 13:01:47 +0100

    Include vk.xml
    Fixes opam build and makes API reproducible.

--- configure.sh
+++ configure.sh
@@ -6,5 +6,5 @@ mkdir -p bin;
 
 mkdir -p econfig && echo "let supported_systems = []" > econfig/econfig.ml;
 
-mkdir -p spec && cd spec \
-&& wget "https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/master/xml/vk.xml"
+#mkdir -p spec && cd spec \
+#&& wget "https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/master/xml/vk.xml"
